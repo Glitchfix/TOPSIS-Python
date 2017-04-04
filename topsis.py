d110f6e530479225eefcf47c297a72a2a50a4b59
Shivanjan Chakravorty <schakravorty846@gmail.com>
2017-04-05 03:24:51 +0530

    Added Database support
    It can now read from a particular Table in a database and can also do some minor testing

--- topsis.py
+++ topsis.py
@@ -1,24 +1,103 @@
 import numpy as np
 
-#Row/Column size
-r,c=[int(i) for i in raw_input("Insert Row and Column: ").split()]	#Row and Columns
+import cx_Oracle
+db=cx_Oracle.connect('username','password')
+cur2=db.cursor()
+cur2.execute('select * from tablename')
+res=cur2.fetchall()
+cur2.close()
+db.close()
 
+
+#Row/Column size
+r=len(res)
+c=len(res[0])
 
 #Preference flags
 cs=[int(i) for i in raw_input("Insert the flags: ").split()]
 
-
-#Weights
+#weights
 wt=np.array([float(i) for i in raw_input("Weight: ").split()])
 
-
+import datetime
 #Matrix foramation
 ar=[]
-for i in range(r):
-    l=[float(i) for i in raw_input("Row "+str(i)+": ").split()]
+for j in range(r):
+    l=[]
+    for i in res[j]:
+	if i.count(':')==0:
+		l.append(float(i))
+	else:
+		time=i.split(':')
+		dt=datetime.timedelta(hours=time[0],minutes=time[1], seconds=time[-1])
+		l.append(float(dt.total_seconds()/3600))
     ar.append(l)
-
+#ar=[list(i) for i in res]
 a=np.array(ar)
+print a
+
+def n_weight(stt):
+    for i in range(len(stt)):
+        if stt[i]>=1 and stt[i]<=2:
+            stt[i]=9
+        elif stt[i]>=3 and stt[i]<=4:
+            stt[i]=7
+        elif stt[i]>=5 and stt[i]<=6:
+            stt[i]=5
+        elif stt[i]>=7 and stt[i]<=8:
+            stt[i]=9
+        else:
+            stt[i]=1
+    return stt
+
+def stt_weight(stt):
+    for i in range(len(stt)):
+        if stt[i]>=1 and stt[i]<=2:
+            stt[i]=9
+        elif stt[i]>=3 and stt[i]<=4:
+            stt[i]=7
+        elif stt[i]>=5 and stt[i]<=6:
+            stt[i]=5
+        elif stt[i]>=7 and stt[i]<=8:
+            stt[i]=9
+        else:
+            stt[i]=1
+    return stt
+
+def du_weight(stt):
+    for i in range(len(stt)):
+        if stt[i]>=1 and stt[i]<=2:
+            stt[i]=9
+        elif stt[i]>=3 and stt[i]<=4:
+            stt[i]=7
+        elif stt[i]>=5 and stt[i]<=6:
+            stt[i]=5
+        elif stt[i]>=7 and stt[i]<=8:
+            stt[i]=9
+        else:
+            stt[i]=1
+    return stt
+
+def de_weight(stt):
+    for i in range(len(stt)):
+        if stt[i]>=1 and stt[i]<=2:
+            stt[i]=9
+        elif stt[i]>=3 and stt[i]<=4:
+            stt[i]=7
+        elif stt[i]>=5 and stt[i]<=6:
+            stt[i]=5
+        elif stt[i]>=7 and stt[i]<=8:
+            stt[i]=9
+        else:
+            stt[i]=1
+    return stt
+
+
+
+
+
+
+
 
 def powpow(sq):
 	if sq==0:
@@ -26,6 +105,23 @@ def powpow(sq):
 	return sq**2
 def sqrt(sq):
 	return sq**0.5
+
+
+nw=n_weight(a[:,0])
+
+sttw=(a[:,3]-a[:,1])/a[:,2]
+sttw=stt_weight(sttw)
+
+duw=a[:,2]
+duw=du_weight(duw)
+
+dew=a[:,3]
+dew=de_weight(dew)
+a[:,0]=nw
+a[:,1]=sttw
+a[:,2]=duw
+a[:,3]=dew
+
 pewpew=np.vectorize(powpow)
 pepe=np.vectorize(sqrt)
 #Step 1(a)
@@ -33,13 +129,13 @@ ap=pewpew(a)
 x=[]
 for i in range(c):
     x.append(sum(ap[:,i])**0.5)
+    
 
 #Step 1(b) and Step 2
 for i in range(c):
 	for j in range(r):
 		a[j,i]*=(wt[i]/x[i])
-    
-
+  
 #Step 3(a) and Step 3(b)
 ast=[]	#A*
 ad=[]	#A'
@@ -50,7 +146,6 @@ for i in range(c):
     else:
         ad.append(min(a[:,i]))
         ast.append(max(a[:,i]))
-
 #Step 4(a)
 ast=np.array([ast]*r)
 ad=np.array([ad]*r)
@@ -60,8 +155,8 @@ a_sd=ad-a
 a_sst=pewpew(a_sst)
 a_sd=pewpew(a_sd)
 
-sst=[]
-sd=[]
+sst=[]  #S*
+sd=[]   #S'
 for i in range(r):
 	u=a_sst[i].sum()
 	p=a_sd[i].sum()
@@ -83,3 +178,17 @@ mx=max(c)
 
 #Answer
 print '\nBest precentile:',mx,"\nBest choice index:",c.index(mx)
+'''for k in range(len(c)):
+    for i in range(k):
+        if c[i]>c[i+1]:
+            temp=c[i]
+            c[i]=c[i+1]
+            c[i+1]=temp'''
+print c
+pin={}
+for i in range(len(c)):
+    pin[c[i]]=i
+c.sort()
+c=c[::-1]
+for i in c:
+    print pin[i]
